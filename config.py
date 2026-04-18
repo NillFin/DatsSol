77d4f5b7da1e25562a63cad36f6a2d852267e14c
Safonenko Aleksandr <likeasassas@gmail.com>
2026-04-18 11:31:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+TOKEN = "6df957c8-91d7-4b36-adfa-aec1bee3c0bd"
+API_BASE = "https://games-test.datsteam.dev"
+
+TURN_TIMEOUT_BUFFER = 0.15  # запас перед следующим ходом
+VISUALIZE = True
